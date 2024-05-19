46adb65b76a7043c9b55bf982f9c53326d47fc86
mamsellem <maurice.amsellem@brainwavegrc.com>
2024-05-19 22:40:09 +0200

    PRI-96 Bug in PrimeReact matrix virtual scroller does not update when the number of cols changes but not the number of rows (refix)

--- components/lib/virtualscroller/VirtualScroller.js
+++ components/lib/virtualscroller/VirtualScroller.js
@@ -562,7 +562,7 @@ export const VirtualScroller = React.memo(
         }, [numToleratedItemsState]);
 
         useUpdateEffect(() => {
-            if (!prevProps.items || prevProps.items.length !== (props.items || []).length || (both && props.items && prevProps.items[0].length !== props.items[0].length)) {
+            if (!prevProps.items || prevProps.items.length !== (props.items || []).length || (both && props.items && prevProps.items[0].length !== (props.items ? props.items[0].length : 0))) {
                 init();
             }
 
